60d556ced4f0d12a2308fcd1eeafe1fa399f57a1
L-Law-Liet <a.kadirov.17.06@gmail.com>
2022-03-19 16:14:32 +0500

    profile update

--- src/app/store/pages/cabinet/cabinet.component.ts
+++ src/app/store/pages/cabinet/cabinet.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../../shared/services/user.service";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {OrderService} from "../../services/order.service";
 import {Order} from "../../models/order.model";
 import {User} from "../../../shared/models/user.model";
+import {EventService} from "../../../shared/services/event.service";
+import {IDictionary} from "../../../shared/interfaces/dictionary";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-cabinet',
@@ -16,17 +19,20 @@ export class CabinetComponent implements OnInit {
   form = new FormGroup({})
   link = ''
   loading = false
+  errors: IDictionary<string> = environment.errors
   constructor(
     private userService: UserService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private eventService: EventService
   ) {
     this.form = new FormGroup({
-      name: new FormControl(userService.user.name),
-      phone: new FormControl(userService.user.phone),
+      name: new FormControl(userService.user.name, Validators.required),
+      phone: new FormControl(userService.user.phone, Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10)])),
     })
   }
 
   ngOnInit(): void {
+    this.eventService.changeBreadcrumbs(['Profile'])
     this.getUser()
     this.getOrders()
   }
@@ -60,7 +66,19 @@ export class CabinetComponent implements OnInit {
       this.loading = true
       this.userService.update(this.form.getRawValue()).subscribe(res => {
         this.userService.setUser(res)
-      }, () => {}, () => {this.loading = false})
+      }, error => {
+        this.loading = false
+        this.form.setErrors(error.error.errors)
+        console.log(error.error.errors, this.form.getError('phone'))
+      }, () => {this.loading = false})
     }
+  }
+  hasError(name: string): boolean {
+    return  this.form.getError(name) || this.form.controls[name].invalid && (this.form.controls[name].dirty || this.form.controls[name].touched)
+  }
+  getError(errors: any) {
+    const key = Object.keys(errors)[0]
+    console.log(errors, key)
+    return this.errors[key]
   }
 }
