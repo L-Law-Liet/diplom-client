9fd7113d873162af88298765a679977b53eacb22
L-Law-Liet <a.kadirov.17.06@gmail.com>
2022-03-19 16:16:45 +0500

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,23 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {IDictionary} from "../app/shared/interfaces/dictionary";
+
 export const environment = {
   production: false,
   API: 'http://diplom.loc/api',
   STORAGE: 'http://diplom.loc/storage/',
   BASE_CURRENCY: 'KZT',
   FREECURRENCY_API_KEY: '98ea91e0-792f-11ec-8dab-17796cf0b74f',
-  FREECURRENCY_API: 'https://freecurrencyapi.net/api/v2/latest',
+  FREECURRENCY_API: 'https://currencyapi.com/api/v2/latest',
   DEFAULT_IMG: 'https://via.placeholder.com/640x480.png/777?text=',
+  errors: {
+    email: 'Invalid email address',
+    mask: 'Invalid field',
+    minlength: 'Must be 10 characters.',
+    maxlength: 'Must be 10 characters.',
+    required: 'This field is required',
+  }
 };
 
 /*
